915205b5619d3a582d4f25474c4ea1f81499505d
valeri zanguri <zanguraa@gmail.com>
2025-02-12 22:32:56 +0400

    add update, getproductbyid, getproductcategory and delete endpoints

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryHandler.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Catalog.API.Products.GetProductByCategory;
+
+public record GetProductByCategoryQuery(string Category) : IQuery<GetProductByCategoryResult>;
+public record GetProductByCategoryResult(IEnumerable<Product> Products);
+internal class GetProductByCategoryQueryHandler 
+    (IDocumentSession session, ILogger<GetProductByCategoryQueryHandler> logger)
+    : IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
+{
+    public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery query, CancellationToken cancellationToken)
+    {
+       logger.LogInformation("GetProductByCategoryQueryHandler.Handle called with {@Query}", query.Category);
+
+        var product = await session.Query<Product>()
+            .Where(p => p.Category.Contains(query.Category))
+            .ToListAsync();
+
+        return new GetProductByCategoryResult(product);
+    }
+}
--- src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -5,7 +5,7 @@ namespace Catalog.API.Products.GetProductById
     public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
     public record GetProductByIdResult(Product Product);
     internal class GetProductByIdQueryHandler
-        (IDocumentSession session, ILogger logger)
+        (IDocumentSession session, ILogger<GetProductByIdQueryHandler> logger)
         : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
     {
         public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
